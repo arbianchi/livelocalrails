e4834076705824c083080893c85ba36b39e60a7b
Adina Bianchi <curiousloop@Adinas-MacBook-Air.local>
2016-07-18 10:07:19 -0400

    filled out users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
 
   def test
-
+    [{
+      username => "username",
+      email => "email@email.com",
+    }]
   end
 
   def show
@@ -11,5 +14,25 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def edit
+    @user = User.find(params[:id]
+  end
+
+  def update
+    @user = User.find params[:id]
+    if @user.update_attributes(user_params)
+      flash[:notice] = "Profile updated!"
+      redirect_to @user
+    else
+      render :edit
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+
 
 end
