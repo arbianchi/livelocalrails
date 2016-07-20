6102bcb1e54511e48fc44aa864046e0c0e4f8b15
Zach H <zdhadgraft@gtcc.edu>
2016-07-20 15:33:21 -0400

    Added first_name and last_name to User table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720191142) do
+ActiveRecord::Schema.define(version: 20160720193047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 20160720191142) do
     t.datetime "updated_at",                          null: false
     t.string   "username",                            null: false
     t.string   "zip_code",                            null: false
+    t.string   "first_name",                          null: false
+    t.string   "last_name",                           null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
     t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
