9e78d5bb540b3f86b2b1e5432f675e0e576f0fb5
Adina Bianchi <curiousloop@Adinas-MacBook-Air.local>
2016-07-20 11:35:59 -0400

    added missing end in User Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,72 +23,73 @@ class UsersController < ApplicationController
     end
   end
 
-def show
-  @user = User.find params[:id]
+  def show
+    @user = User.find params[:id]
 
-  respond_to do |format|
-    format.json {render json: user_data }
-    format.html { not_found }
-  end
-end
-
-def index
-  @users = User.all
-
-  respond_to do |format|
-    format.json {render json: user_data }
-    format.html { not_found }
-  end
-end
-
-def edit
-  @user = User.find(params[:id])
-end
-
-def update
-  @user = User.find params[:id]
-
-  if @user.update_attributes(user_params)
-    flash[:notice] = "Profile updated!"
-    redirect_to @user
-  else
-    render :edit
+    respond_to do |format|
+      format.json {render json: user_data }
+      format.html { not_found }
+    end
   end
 
-  private
+  def index
+    @users = User.all
 
-  def set_token ops={}
-    username = User.find(ops[:user].id).username
-    return {"token": username}
+    respond_to do |format|
+      format.json {render json: user_data }
+      format.html { not_found }
+    end
   end
 
-  def user_data
-    [{
-      :id => 1,
-      :username => "username",
-      :first_name => "Joe",
-      :last_name => "Shmo",
-      :email => "email@email.com",
-      :zipcode => "27701",
-      :survey => []
-    }]
+  def edit
+    @user = User.find(params[:id])
   end
 
-  def user_params
-    {
-      "username"    => params[:username],
-      "email"       => params[:email],
-      "password"    => params[:password],
-      "zip_code"    => params[:zip_code]
-    }
+  def update
+    @user = User.find params[:id]
 
-#    params.permit(
-#      :username,
-#      :email,
-#      :password,
-#      :first_name,
-#      :last_name,
-#      :zip_code
-#    )
+    if @user.update_attributes(user_params)
+      flash[:notice] = "Profile updated!"
+      redirect_to @user
+    else
+      render :edit
+    end
+
+    private
+
+    def set_token ops={}
+      username = User.find(ops[:user].id).username
+      return {"token": username}
+    end
+
+    def user_data
+      [{
+        :id => 1,
+        :username => "username",
+        :first_name => "Joe",
+        :last_name => "Shmo",
+        :email => "email@email.com",
+        :zipcode => "27701",
+        :survey => []
+      }]
+    end
+
+    def user_params
+      {
+        "username"    => params[:username],
+        "email"       => params[:email],
+        "password"    => params[:password],
+        "zip_code"    => params[:zip_code]
+      }
+
+      #    params.permit(
+      #      :username,
+      #      :email,
+      #      :password,
+      #      :first_name,
+      #      :last_name,
+      #      :zip_code
+      #    )
+    end
   end
 end
