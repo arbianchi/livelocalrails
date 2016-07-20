8daff176402f6cabc76c46a8b6ce2de0576b1493
Zach H <zdhadgraft@gtcc.edu>
2016-07-20 17:53:07 -0400

    Added users#sign_in controller spec.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  let(:user) { create(:user, password: "password") }
 
+  it "can sign up for an account"
+
+  it "can sign in to an account" do
+    u = user
+
+    post :sign_in, {username: u.username, password: "password"}
+    expect(response.code).to eq("200")
+  end
 end
