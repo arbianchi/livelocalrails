6b9b154d96791b696ef925d4ca8b1def387ea1f6
Zach H <zdhadgraft@gtcc.edu>
2016-07-22 09:19:03 -0400

    Added helper function to prep yelp data for database.

--- app/services/yelp_gem_wrapper.rb
+++ app/services/yelp_gem_wrapper.rb
@@ -11,4 +11,19 @@ module YelpGemWrapper
     limit       = 20    # hard limit set by yelp
     Yelp.client.search(location, {limit: limit, term: term}).businesses
   end
+
+  def self.package_business b
+      {
+        address:      b.first.location.address.join(" "),
+        zip_code:     b.first.location.postal_code,
+        phone:        b.first.phone,
+        city:         b.first.location.city,
+        location:     [b.first.location.coordinate.latitude,
+                       b.first.location.coordinate.longitude],
+        image_url:    b.first.snippet_image_url,
+        website_url:  b.first.url,
+        categories:   b.first.categories.join(","),
+        yelp_id:      b.first.id
+      }
+  end
 end
