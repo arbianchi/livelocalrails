b585155831cadbf90288e016210ea96098662e52
Zach H <zdhadgraft@gtcc.edu>
2016-07-24 12:09:59 -0400

    Added helper module for use in all tests; created business factory.

--- spec/factories/businesses.rb
+++ spec/factories/businesses.rb
@@ -1,5 +1,12 @@
 FactoryGirl.define do
   factory :business do
-    
+    name {      "FAKEBIZ: " + Faker::Company.name }
+    address {   Faker::Address.street_address }
+    city {      Faker::Address.city }
+    zip_code {  Faker::Address.zip_code }
+    phone {     Faker::PhoneNumber.phone_number }
+    location {  [Faker::Address.latitude,
+                Faker::Address.longitude] }
+    yelp_id {   Faker::Code.asin }
   end
 end
