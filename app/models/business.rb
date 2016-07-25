d55909b4e32e4d684339ee122378129e4ba6fb9d
Adina Bianchi <curiousloop@Adinas-MacBook-Air.local>
2016-07-25 13:08:57 -0400

    tests pass

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,6 +1,5 @@
 class Business < ApplicationRecord
   belongs_to :owner, class_name: "User", foreign_key: "owner_id", optional: true
 
-  has_many :questions
   has_many :surveys, as: :responder
 end
