35aac8d87d627c3f313c7e86ac19941195d38ace
Adina Bianchi <curiousloop@Adinas-MacBook-Air.local>
2016-07-26 12:48:01 -0400

    index and create working with tests

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -5,29 +5,28 @@ RSpec.describe QuestionsController, type: :controller do
   include Helpers
 
   let(:user) { create :user }
-  let(:business) { Business.create owner_id: user.id, name: 'fakebiz', phone: '9196800770'   }
-  let(:question) { Question.create user_id: user.id, business_id: business.id, question_text: "Will this feature work correctly?" }
-
-  def valid_params
-    { user_id: user.id,
-      business_id: user.business.id, 
-      question_text: 'question?' 
-    }
-  end
+  let(:business) { create :business }  
+  let(:question) { create :question }
 
   describe "GET #index" do
-    xit "returns list of posted questions for a business" do
+    it "returns list of posted questions for a business" do
 
       set_auth_header user
 
-      business.save!
-      question.save!
+      b = create :business, owner_id: user.id
+      q1 = create :question, user_id: user.id, business_id: business.id
+      q2 = create :question, user_id: user.id, business_id: business.id
+
+      b.save!
+      q1.save!
+      q2.save!
 
       get :index, params:{ business_id: business.id }
 
       expect(response).to have_http_status(:ok)
       expect( parsed_response.first["user_id"] ).to eq(user.id)
       expect(parsed_response.class).to eq(Array)
+      expect(parsed_response.count).to eql(2)
 
     end
   end
@@ -36,11 +35,13 @@ RSpec.describe QuestionsController, type: :controller do
     it "posts question to business" do
 
       set_auth_header user
-      business.save!
-      post :create, params: valid_params
+      b = create :business, owner_id: user.id
+      b.save!
 
       expect { 
-        post :create, params: valid_params
+        post :create, params: 
+    { user_id: user.id,
+      business_id: user.business.id, }
       }.to change(Question, :count).by(1)
 
       expect(response).to have_http_status(:ok)
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :question do
+
+    question_text { "But why?" }
     
   end
 end
