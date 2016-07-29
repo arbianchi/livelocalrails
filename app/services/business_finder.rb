c0c0c2a4dc605d207cb5c0f70bcbe66de50eaf36
Zach H <zdhadgraft@gtcc.edu>
2016-07-29 15:02:49 -0400

    WIP preparing to implement paging of results.

--- app/services/business_finder.rb
+++ app/services/business_finder.rb
@@ -1,4 +1,4 @@
-STRATEGIES = [GoogleStrategy, YelpStrategy]
+STRATEGIES = [GoogleStrategy] #, YelpStrategy]
 
 class BusinessFinder
 
@@ -7,12 +7,22 @@ class BusinessFinder
   end
 
   def run ops={}
-    STRATEGIES.each do |strategy|
-      o = strategy.new(ops)
-      o.create_businesses!
-      @results += o.businesses
-      # break_if @results.count > 100
+#    STRATEGIES.each do |strategy|
+#      o = strategy.new(ops)
+#      o.create_businesses!
+#      @results += o.businesses
+#      binding.pry
+#      # break_if @results.count > 100
+    #    end
+
+    binding.pry
+    22.times do
+      @results.push(FactoryGirl.create :business)
     end
     return @results
+  end
+
+  def page_results
+    binding.pry
   end
 end
