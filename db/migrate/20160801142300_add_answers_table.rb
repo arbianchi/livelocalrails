357a071c217e0cb83e972f7386dc73871e52fb03
Adina Bianchi <curiousloop@Adinas-MacBook-Air.local>
2016-08-01 10:34:56 -0400

    regenerated answers table with proper data types

--- db/migrate/20160801142300_add_answers_table.rb
+++ db/migrate/20160801142300_add_answers_table.rb
@@ -1,10 +1,11 @@
-class CreateAnswers < ActiveRecord::Migration[5.0]
+class AddAnswersTable < ActiveRecord::Migration[5.0]
   def change
+
     create_table :answers do |t|
 
       t.integer :question_id
       t.integer :answerer_id
-      t.integer :answerer_type
+      t.string :answerer_type
       t.string :answer_text
 
       t.timestamps
