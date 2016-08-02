9191728cb88b494ed176b18caef17a8fb10e93b4
Adina Bianchi <curiousloop@Adinas-MacBook-Air.local>
2016-08-02 12:20:05 -0400

    reset mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { :host => 'livelocalrails.herokuapp.com' }
-  Rails.application.routes.default_url_options[:host] = 'livelocalrails.herokuapp.com'
+  # config.action_mailer.default_url_options = { :host => 'livelocalrails.herokuapp.com' }
+  # Rails.application.routes.default_url_options[:host] = 'livelocalrails.herokuapp.com'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -96,21 +96,21 @@ Rails.application.configure do
   #   :authentication => :plain,
   # }
 
-  config.time_zone = 'Eastern Time (US & Canada)'
+  # config.time_zone = 'Eastern Time (US & Canada)'
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.default :charset => "utf-8"
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.mailgun.org",
-    port: 587,
-    domain: ENV["domain"],
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["username"],
-    password: ENV["password"]
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.mailgun.org",
+  #   port: 587,
+  #   domain: ENV["domain"],
+  #   authentication: "plain",
+  #   enable_starttls_auto: true,
+  #   user_name: ENV["username"],
+  #   password: ENV["password"]
+  # }
 
 end
